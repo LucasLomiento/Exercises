539d405807a60a4bf2909086a3264fa0fe5243c8
Lucas Lomiento <lucaslomiento@icloud.com>
2022-08-22 20:28:49 -0300

    update de trabalho

--- Trybe/fundamentos/b4-intro-js-logica/b4.3/index.js
+++ Trybe/fundamentos/b4-intro-js-logica/b4.3/index.js
@@ -3,3 +3,14 @@ for(let i = 2; i <= 150; i++){
     (i % 3 == 0) ? count++ : count;
 }
 console.log((count == 50) ? "50 números e essa mensagem é secreta hem..." : count," números" )
+
+//Verificar a maior palavra dentro de uma array
+let arrayPalavras = ["Lucas", "Joao", "Carvalho"];
+let indexMaiorPalavra = 0;
+
+for(let i = 0; i < arrayPalavras.length; i++){
+    if(arrayPalavras[i].length > arrayPalavras[indexMaiorPalavra].length){
+        indexMaiorPalavra = i;
+    }
+}
+console.log(arrayPalavras[indexMaiorPalavra])
